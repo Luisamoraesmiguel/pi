daf9a841d1db93cebdb026590661cc4de99c4d14
Sabrina Prates <sabrina.ps@puccampinas.edu.br>
2026-04-08 12:37:57 -0300

    Código igual ao da Joice

--- teste_conexao.py
+++ teste_conexao.py
@@ -1,15 +1,12 @@
 import mysql.connector
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 conexao = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_DATABASE")
+    host='127.0.0.1',
+    user='root',
+    password='sua_senha',
+    database='tabela_bd'
 )
+
 cursor = conexao.cursor()
 
 sql = "INSERT INTO eleitores (nome, cpf, titulo, mesario, votou, chave_de_acesso) VALUES (%s, %s, %s, %s, %s, %s)"
@@ -18,13 +15,5 @@ cursor.execute(sql, valores)
 conexao.commit()
 
 print("Cadastrado com sucesso!")
-
-    
-    
-
-
-
-
-
 
 
