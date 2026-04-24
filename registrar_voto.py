6da3b7cd50b047ee83751cde9b8d50bc885cb20f
Sabrina Prates <sabrina.ps@puccampinas.edu.br>
2026-04-24 00:04:35 -0300

    algumas alterações no código de registrar voto no banco de dados  e menu-  1 hora

--- registrar_voto.py
+++ registrar_voto.py
@@ -1,33 +1,40 @@
 import mysql.connector
+from datetime import datetime
 
 # Essa é a função que o professor quer
 def gravar_voto_no_banco(numero_escolhido):
-    """
-    Guarda o voto do candidato no banco de dados.
-    """
-    try:
-        # 1. Conecta no MySQL (Porta de entrada)
-        conexao = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="tabela_bd"
+    #try:
+        # 1. Abre a conexao 
+    conexao = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="tabela_bd"
         )
-        cursor = conexao.cursor()
+    cursor = conexao.cursor()
 
-        # 2. Comando para INSERIR o dado
-        comando_sql = "INSERT INTO votos (numero_candidato) VALUES (%s)"
-        cursor.execute(comando_sql, (numero_escolhido,))
+    # Pegar data e hora do voto
+    data_hora_voto = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # Formata a data e hora para o formato do MySQL
+    
+    #Registrar voto na tabela de votos
+    comando_sql = "INSERT INTO votos (numero_candidato, data_hora_voto) VALUES (%s, %s)"
+    cursor.execute(comando_sql, (numero_escolhido, data_hora_voto)) # Registra o voto na tabela de votos
 
-        # 3. Salva a mudança (Obrigatório)
-        conexao.commit()
-        print("Sucesso: Voto registrado!")
+    # Atualizar os status do eleitor
+    sql_status = "UPDATE eleitores SET votou = 'S' WHERE titulo = %s"
+    cursor.execute(sql_status, (titulo_eleitor,)) # Atualiza o status do eleitor para "votou = 'S'"
+    
+    # Salvar as mudanças no banco de dados
+    conexao.commit()
+    print("Sucesso: Voto registrado!")
+    print(f"Protocolo: {data_hora_voto} - Candidato: {numero_escolhido}")
 
-    except:
+    #except:
         # Se der erro (ex: banco desligado), avisa aqui
-        print("Erro: Nao foi possivel salvar o voto.")
+        #print("Erro: Nao foi possivel salvar o voto.")
 
-    finally:
+    #finally:
         # 4. Fecha a conexão sempre
-        conexao.close()
+    cursor.close()
+    conexao.close()
 
