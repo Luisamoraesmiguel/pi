aff66337e799a41a2ba6dfedf0ec01fd0a1ee247
Sabrina Prates <sabrina.ps@puccampinas.edu.br>
2026-04-29 19:28:40 -0300

    mudei para funcao

--- Códigos_fonte/modulo_voto.py
+++ Códigos_fonte/modulo_voto.py
@@ -1,18 +1,18 @@
 from conexao import conectar
 
+def realizar_votacao():
+    conexao = conectar()
+    cursor = conexao.cursor()
 
-conexao = conectar()
-cursor = conexao.cursor()
-
-titulo=input("Digite o seu Título de Eleitor: ")
-voto=input("Digite o número do seu candidato: ")
+    titulo=input("Digite o seu Título de Eleitor: ")
+    voto=input("Digite o número do seu candidato: ")
 
     #Comando pro Banco de dados
-comando_sql = "INSERT INTO votos (titulo, numero, data_voto) VALUES (%s, %s, NOW())"
-dados_voto = (titulo, voto)
-cursor.execute(comando_sql, dados_voto)
-conexao.commit()
-print(f"Voto confirmado para o candidato {voto}!")
-cursor.close()
-conexao.close()
-realizar_votacao()
+    comando_sql = "INSERT INTO votos (titulo, numero, data_voto) VALUES (%s, %s, NOW())"
+    dados_voto = (titulo, voto)
+    cursor.execute(comando_sql, dados_voto)
+    conexao.commit()
+    print(f"Voto confirmado para o candidato {voto}!")
+    cursor.close()
+    conexao.close()
+    
