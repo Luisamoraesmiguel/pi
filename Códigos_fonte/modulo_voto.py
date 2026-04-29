c0909a854368f16379269ccff7b175944cabc11c
Sabrina Prates <sabrina.ps@puccampinas.edu.br>
2026-04-29 19:20:32 -0300

    mudado comando de conexao com banco de dados para funcao 1 min

--- Códigos_fonte/modulo_voto.py
+++ Códigos_fonte/modulo_voto.py
@@ -1,23 +1,18 @@
-import mysql.connector
+from conexao import conectar
 
-def realizar_votacao():
-    conexao= mysql.connector.connect(
-        host="localhost", 
-        user="root", 
-        password="", 
-        database="empresa_db")
-    
-    cursor = conexao.cursor()
 
-    titulo=input("Digite o seu Título de Eleitor: ")
-    voto=input("Digite o número do seu candidato: ")
+conexao = conectar()
+cursor = conexao.cursor()
+
+titulo=input("Digite o seu Título de Eleitor: ")
+voto=input("Digite o número do seu candidato: ")
 
     #Comando pro Banco de dados
-    comando_sql = "INSERT INTO votos (titulo, numero, data_voto) VALUES (%s, %s, NOW())"
-    dados_voto = (titulo, voto)
-    cursor.execute(comando_sql, dados_voto)
-    conexao.commit()
-    print(f"Voto confirmado para o candidato {voto}!")
-    cursor.close()
-    conexao.close()
+comando_sql = "INSERT INTO votos (titulo, numero, data_voto) VALUES (%s, %s, NOW())"
+dados_voto = (titulo, voto)
+cursor.execute(comando_sql, dados_voto)
+conexao.commit()
+print(f"Voto confirmado para o candidato {voto}!")
+cursor.close()
+conexao.close()
 realizar_votacao()
