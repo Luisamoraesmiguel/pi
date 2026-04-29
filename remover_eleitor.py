da07d97ba49aa05ad8c1ffc649265342e4b6447c
Sabrina Prates <sabrina.ps@puccampinas.edu.br>
2026-04-28 22:46:17 -0300

    pequenas alteracoes

--- remover_eleitor.py
+++ remover_eleitor.py
@@ -9,18 +9,18 @@ def apagar_eleitor_do_banco(titulo_digitado):
     cursor = conexao.cursor()
 
         # 2. Comando para DELETAR (Onde o titulo for igual ao digitado)
-        comando_sql = "DELETE FROM eleitores WHERE titulo = %s"
-        cursor.execute(comando_sql, (titulo_digitado,))
+    comando_sql = "DELETE FROM eleitores WHERE titulo = %s"
+    cursor.execute(comando_sql, (titulo_digitado,))
 
         # 3. Salva a mudanca
-        conexao.commit()
-        print(f"\n[SUCESSO] Eleitor com título {titulo_digitado} removido com sucesso!")
+    conexao.commit()
+    print(f"\n[SUCESSO] Eleitor com título {titulo_digitado} removido com sucesso!")
 
     #except Exception as e:
         #print(f"Erro ao remover: {e}")
 
     #finally:
         #if conexao.is_connected():
-        cursor.close() # Fecha o cursor
-        conexao.close()
+    cursor.close() # Fecha o cursor
+    conexao.close()
 
