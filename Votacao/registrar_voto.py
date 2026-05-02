f862a747334f3103f14c728b280caacae1ab61ab
Luisamoraesmiguel <luisa.mm1@puccampinas.edu.br>
2026-05-02 13:00:44 -0300

    Update registrar_voto.py
    formatei 5 min

--- Votacao/registrar_voto.py
+++ Votacao/registrar_voto.py
@@ -6,16 +6,16 @@ def gravar_voto_no_banco(numero_escolhido, titulo_eleitor, data_hora_voto):
     conexao = conectar()
     cursor = conexao.cursor()
 
-    data_hora_voto = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # Formata a data e hora para o formato do MySQL
+    data_hora_voto = datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
     
     comando_sql = "INSERT INTO votos (numero_candidato, data_hora_voto) VALUES (%s, %s)"
-    cursor.execute(comando_sql, (numero_escolhido, data_hora_voto)) # Registra o voto na tabela de votos
+    cursor.execute(comando_sql, (numero_escolhido, data_hora_voto)) 
 
     sql_status = "UPDATE eleitores SET votou = 'S' WHERE titulo = %s"
-    cursor.execute(sql_status, (titulo_eleitor,)) # Atualiza o status do eleitor para "votou = 'S'"
+    cursor.execute(sql_status, (titulo_eleitor,)) 
     
     conexao.commit()
-    print("Sucesso: Voto registrado!")
+    print("\nSucesso: Voto registrado!")
     print(f"Protocolo: {data_hora_voto} - Candidato: {numero_escolhido}")
 
     cursor.close()
