c9cc788f5876731d807c74ee611c6ddd1b984a96
Luisamoraesmiguel <luisa.mm1@puccampinas.edu.br>
2026-05-02 12:49:49 -0300

    Refactor zerezima function for clarity and consistency
    Removi comentários

--- Códigos_fonte/zerezima.py
+++ Códigos_fonte/zerezima.py
@@ -3,28 +3,27 @@ import os
 import mysql.connector
 
 def zerezima():
-    os.system('clear')  # Limpa a tela para melhor visualização
+    os.system('clear')  
     print("\n== ZEREZIMA ==")
 
 
     conexao = conectar()
     cursor = conexao.cursor()
 
-    #Zerando os votos dos candidatos
     limpar = "DELETE FROM votos"
     cursor.execute(limpar)
     conexao.commit()
 
     relatorio = "SELECT Nome, Num_votacao FROM candidatos"
     cursor.execute(relatorio)
-    lista_candidatos = cursor.fetchall() # Pega a lista de candidatos para o relatório
+    lista_candidatos = cursor.fetchall() 
     print("Candidatos:")
     for candidato in lista_candidatos:
         print(f"Candidato: {candidato[0]} | Número: {candidato[1]} | Votos: 0")
     print("\nZerezima realizada com sucesso!")
     
-    cursor.close() # Fecha o cursor
-    conexao.close() # Fecha a conexão com o banco de dados
+    cursor.close() 
+    conexao.close() 
 
     input("\nPressione Enter para liberar a urna")
     return True
