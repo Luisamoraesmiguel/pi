84b350415ab69fe71174b572a43d5af3a1b5a06a
Luisamoraesmiguel <luisa.mm1@puccampinas.edu.br>
2026-05-02 12:57:58 -0300

    Update Abertura.py
    Formatei 10 min

--- Votacao/Abertura.py
+++ Votacao/Abertura.py
@@ -7,7 +7,7 @@ def abertura_votacao():
     print("\n== ABERTURA DO SISTEMA DE VOTAÇÃO ==")
 
     print("\n IDENTIFICAÇÃO DO MESÁRIO")
-    titulo = input("Digite o número do título de eleitor do mesário: ")
+    titulo = input("\nDigite o número do título de eleitor do mesário: ")
     cpf = input("Digite os 4 primeiros dígitos do CPF do mesário: ")
     chave = input("Digite a chave de acesso do mesário: ").upper().strip()
 
@@ -17,14 +17,14 @@ def abertura_votacao():
         if N == 'N' or N == 'n':
             print("Retornando ao menu do sistema de votação...")
             return
-        titulo = input("Digite o número do título de eleitor do mesário: ")
+        titulo = input("\nDigite o número do título de eleitor do mesário: ")
         cpf = input("Digite os 4 primeiros dígitos do CPF do mesário: ")
         chave = input("Digite a chave de acesso do mesário: ").upper().strip()
 
-    print("Mesário identificado com sucesso!")
+    print("\nMesário identificado com sucesso!")
     input("Pressione Enter para realizar a Zerezima")
 
     while not zerezima():
-        print("Erro ao realizar a Zerezima. Por favor, tente novamente.")
+        print("\nErro ao realizar a Zerezima. Por favor, tente novamente.")
         input("Pressione Enter para realizar a Zerezima")
-    print("Zerezima realizada com sucesso!")
+    print("\nZerezima realizada com sucesso!")
