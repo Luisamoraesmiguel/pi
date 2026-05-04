1e2caf252abe187878002ac15c0cb9b43dd9a205
Arthur Peripolli da Silva <arthur.ps3@puccampinas.edu.br>
2026-05-04 17:15:21 -0300

    reformulei "registrar_voto" - 20min

--- Votacao/registrar_voto.py
+++ Votacao/registrar_voto.py
@@ -1,37 +1,37 @@
+from Códigos_fonte import gerador_protocolo as protocolo
 from conexao import conectar
 from datetime import datetime
+import Criptografia
 
-def gravar_voto_no_banco(numero_escolhido, titulo_eleitor, data_hora_voto):
-
+def gravar_voto_no_banco(numero_escolhido, titulo_eleitor): 
     conexao = conectar()
     cursor = conexao.cursor()
 
-    data_hora_voto = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Obtém a data e hora atual no formato adequado para o banco de dados
     
-    cursor.execute("SELECT id FROM candidatos WHERE numero = %s", (numero_escolhido,))
-    resultado = cursor.fetchone()
-    if resultado is None:
-        print('Eleitor não encontrado.')
-        cursor.close()
-        conexao.close()
-        return
+    protocolo_limpo = protocolo.criar_novo_protocolo(numero_escolhido)
+    protocolo_cifrado = Criptografia.cifrar(protocolo_limpo)
 
-    if resultado[0]== 'S':
-        print('Eleitor já votou.')
-        cursor.close()
-        conexao.close()
-        return
+   
+    agora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
+    # Verifica se o candidato existe 
+    cursor.execute("SELECT nome FROM candidatos WHERE numero = %s", (numero_escolhido,))
+    if cursor.fetchone() is None and numero_escolhido != "00": 
+        print('Candidato não encontrado. Voto será computado como Nulo.')
+
+    # 4. Grava o voto na tabela de votos
     comando_sql = "INSERT INTO votos (Candidato, Datahora, Protocolo_votacao) VALUES (%s, %s, %s)"
-    cursor.execute(comando_sql, (numero_escolhido, data_hora_voto, data_hora_voto)) 
+    cursor.execute(comando_sql, (numero_escolhido, agora, protocolo_cifrado)) 
 
+    
     sql_status = "UPDATE eleitores SET votou = 'S' WHERE titulo = %s"
     cursor.execute(sql_status, (titulo_eleitor,)) 
     
     conexao.commit()
     print("\nSucesso: Voto registrado!")
-    print(f"Protocolo: {data_hora_voto} - Candidato: {numero_escolhido}")
+    print(f"Protocolo: {protocolo_limpo} - Candidato: {numero_escolhido}")
 
     cursor.close()
     conexao.close()
 
+    return protocolo_limpo
