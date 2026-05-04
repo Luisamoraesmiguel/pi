bea48869d98d8c3c84cdf3d29037f9bcb76689c3
Sabrina Prates <sabrina.ps@puccampinas.edu.br>
2026-05-03 22:57:03 -0300

    arrumados nomes das colunas

--- Votacao/registrar_voto.py
+++ Votacao/registrar_voto.py
@@ -6,10 +6,24 @@ def gravar_voto_no_banco(numero_escolhido, titulo_eleitor, data_hora_voto):
     conexao = conectar()
     cursor = conexao.cursor()
 
-    data_hora_voto = datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
+    data_hora_voto = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Obtém a data e hora atual no formato adequado para o banco de dados
     
-    comando_sql = "INSERT INTO votos (numero_candidato, data_hora_voto) VALUES (%s, %s)"
-    cursor.execute(comando_sql, (numero_escolhido, data_hora_voto)) 
+    cursor.execute("SELECT id FROM candidatos WHERE numero = %s", (numero_escolhido,))
+    resultado = cursor.fetchone()
+    if resultado is None:
+        print('Eleitor não encontrado.')
+        cursor.close()
+        conexao.close()
+        return
+
+    if resultado[0]== 'S':
+        print('Eleitor já votou.')
+        cursor.close()
+        conexao.close()
+        return
+    
+    comando_sql = "INSERT INTO votos (Candidato, Datahora, Protocolo_votacao) VALUES (%s, %s, %s)"
+    cursor.execute(comando_sql, (numero_escolhido, data_hora_voto, data_hora_voto)) 
 
     sql_status = "UPDATE eleitores SET votou = 'S' WHERE titulo = %s"
     cursor.execute(sql_status, (titulo_eleitor,)) 
